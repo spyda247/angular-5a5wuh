5dd5741980e50448bf9527b726097a4c61cf2c99
spyda247 <babajideibiayo@yahoo.com>
2020-07-12 09:32:23 +0100

    4 files modified - Cleaned up styles for parent and child components

--- src/app/flop-chooser/flop-chooser.component.ts
+++ src/app/flop-chooser/flop-chooser.component.ts
@@ -4,14 +4,17 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'my-flop-chooser',
   templateUrl: './flop-chooser.component.html',
-  styleUrls: [ ]
+  styles: [
+    'div { cursor:pointer; }',
+    'img { width:8rem; height:auto; }'
+  ]
 })
 export class FlopChooserComponent  {
   @Input() movie
   @Output() choose = new EventEmitter();
 
   onClick() {
-    this.choose.emit()
+    this.choose.emit(this.movie)
   }
 
 }
